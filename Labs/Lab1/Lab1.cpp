5d3c7929ecef10f743417fda98096d613ea84b80
RootCellar <rootcellar9877@gmail.com>
2020-08-31 10:33:38 -0800

    Lab 01

---
+++ Labs/Lab1/Lab1.cpp
@@ -0,0 +1,19 @@
+
+/*
+* Darian Marvel
+* 8/31/2020
+* Lab1.cpp
+* Lab 01 for CS 201
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    std::cout << "Hello World!" << endl;
+
+    return 0;
+}
--- Labs/folders.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..36}; do
-mkdir LAB$i
-cd LAB$i
-touch .ignore
-cd ..
-done
