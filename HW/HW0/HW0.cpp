bcd022cd5fa882538ae6fb701e6bd73b94f1a1a6
RootCellar <rootcellar9877@gmail.com>
2020-08-31 10:37:33 -0800

    Starting HW0

--- HW/HW0/HW0.cpp
+++ HW/HW0/HW0.cpp
@@ -1,18 +1,18 @@
 /*
 * Darian Marvel
 * 8/31/2020
-* Lab1.cpp
-* Lab 01 for CS 201
+* HW0.cpp
+* Doing HW0 for CS 201
 */
 
+//includes
 #include <iostream>
 
+//using statements
 using std::cout;
 using std::endl;
 
 int main()
 {
-    std::cout << "Hello World!" << endl;
-
-    return 0;
+    cout << "Hello World!" << endl;
 }
