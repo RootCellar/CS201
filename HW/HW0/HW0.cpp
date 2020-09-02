6e7758c676f600bd205e26e2afe0886a6624ca33
RootCellar <rootcellar9877@gmail.com>
2020-09-01 17:58:28 -0800

    Print 5 times, add comments

--- HW/HW0/HW0.cpp
+++ HW/HW0/HW0.cpp
@@ -12,7 +12,16 @@
 using std::cout;
 using std::endl;
 
+
+//Main function
 int main()
 {
-    cout << "Hello World!" << endl;
+    //cout << "Hello World!" << endl;
+
+    //Loop 5 times, running the code inside each time
+    for (int i = 0; i < 5; i++) {
+        cout << "Hello, World!" << endl; //Print "Hello, World!"
+    }
+
+    return 0; //End the program with code 0
 }
