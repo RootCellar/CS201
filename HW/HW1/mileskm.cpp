9c1914966b7912509dfa936e5a7961d3004b8583
RootCellar <rootcellar9877@gmail.com>
2020-09-11 18:22:53 -0800

    Complete mileskm

--- HW/HW1/mileskm.cpp
+++ HW/HW1/mileskm.cpp
@@ -5,6 +5,9 @@
  * Converting miles to kilometers for CS 201 HW 1
  */
 
+//Place conversion factor here
+#define MITOKM 1.609
+
 #include <iostream>
 
 using std::cout;
@@ -13,4 +16,28 @@ using std::endl;
 
 int main() {
 
+	double numMiles = 0;
+
+	while (true) {
+
+		cout << "Enter a positive number to convert to miles, 0 to quit: ";
+		cin >> numMiles;
+		cout << endl;
+
+		if (numMiles == 0) {
+			cout << "Ok. Bye!" << endl;
+			break;
+		}
+
+		if (numMiles < 0) {
+			cout << "Please enter a valid number." << endl;
+			continue;
+		}
+
+		double km = numMiles * MITOKM;
+
+		cout << numMiles << " miles to kilometers is: " << km << endl;
+	}
+
+	return 0;
 }
