b7721120c7e1d847f546db6e8d506350cf12a11c
RootCellar <RootCellar9877@GMail.com>
2020-09-15 17:36:01 -0800

    Complete Lab 4

--- Labs/Lab4/Lab4.cpp
+++ Labs/Lab4/Lab4.cpp
@@ -7,7 +7,17 @@
 
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
 int main()
 {
-    
+    for(int i=1; i<=100; i++) {
+	
+	if(i%3 == 0) {
+	    cout << "Fizz" << endl;
+	}
+	else cout << i << endl;
+	
+    }
 }
