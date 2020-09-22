f4777d99b21c8a579525610b3d6d4a0e5eb4d6e0
RootCellar <rootcellar9877@gmail.com>
2020-09-21 19:25:07 -0800

    Complete functions, main function

--- HW/HW2/HW2.cpp
+++ HW/HW2/HW2.cpp
@@ -33,11 +33,52 @@ int main()
 {
     vector<string> names;
 
+    //Get list of names
     InputNames(names);
+    cout << endl;
+
+    //Check if some arbitrary name exists
+    bool doesExist = DoesNameExist("James", names);
+    debug("Seeing if James is on the list...");
+    if (doesExist) cout << "True";
+    else cout << "False";
+    cout << endl;
+    cout << endl;
+
+    //Print names in list
+    PrintNames(names);
+
+    //Total length of names in list
+    getTotalNameLength(names);
 }
 
 void debug(const string out) {
     cout << DEBUG_PREFIX << " " << out << endl;
+}
+
+int getTotalNameLength(const vector<string>& names) {
+    int total = 0;
+    for (int i = 0; i < names.size(); i++) {
+        total += names.at(i).size();
+        cout << names.at(i) << " : " << names.at(i).size() << endl;
+    }
+    cout << "Total: " << total << endl;
+    cout << endl;
+    return total;
+}
+
+void PrintNames(const vector<string>& names) {
+    cout << "Names in the list: " << endl;
+    for (int i = 0; i < names.size(); i++) {
+        cout << names.at(i) << endl;
+    }
+}
+
+bool DoesNameExist(const string& name, const vector<string> names) {
+    for (int i = 0; i < names.size(); i++) {
+        if (names.at(i) == name) return true;
+    }
+    return false;
 }
 
 void InputNames(vector<string>& names) {
@@ -47,6 +88,7 @@ void InputNames(vector<string>& names) {
         cin >> in;
         cout << endl;
         if (in.size() < 2) break;
+        debug("Putting name in list...");
         names.push_back(in);
         cout << "Now have " << names.size() << " names." << endl; //Kinda debug, kinda tell them how many names are in the list
     }
