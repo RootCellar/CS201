5deb0051699518077860acb235bb5bb89b0bd4e3
RootCellar <rootcellar9877@gmail.com>
2020-09-28 17:51:45 -0800

    Set up collatz

--- HW/HW3/build.sh
+++ HW/HW3/build.sh
@@ -1,2 +1,4 @@
 #HW main program
 g++ -o HW3 HW3.cpp boxer.cpp
+
+g++ -o collatz collatz.cpp
---
+++ HW/HW3/collatz.cpp
@@ -0,0 +1,18 @@
+/*
+ * Darian Marvel
+ * 9/28/2020
+ * collatz.cpp
+ * Letting the user try a collatz sequence
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void collatz(int n);
+
+int main() {
+  
+}
