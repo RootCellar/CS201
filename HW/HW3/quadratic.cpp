ba1c1918515f38c76d9c7add924b5c48de3612d9
RootCellar <rootcellar9877@gmail.com>
2020-09-28 18:29:29 -0800

    Help program check if the equation has any real roots

--- HW/HW3/quadratic.cpp
+++ HW/HW3/quadratic.cpp
@@ -11,6 +11,16 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+bool canSolve(double a, double b, double c);
+
 int main() {
 
+}
+
+//Just check if this equation has any real roots
+bool canSolve(double a, double b, double c) {
+  double left = b * b;
+  double right = 4 * a * c;
+  if(left-right < 0) return false;
+  else return true;
 }
