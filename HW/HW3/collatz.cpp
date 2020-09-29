a9ec0f4d59916703a5ce65ef0c5dace123822f3e
RootCellar <rootcellar9877@gmail.com>
2020-09-28 17:55:47 -0800

    Implement collatz function to make very easy to simulate the function itself, and test it

--- HW/HW3/collatz.cpp
+++ HW/HW3/collatz.cpp
@@ -14,5 +14,18 @@ using std::endl;
 void collatz(int n);
 
 int main() {
-  
+  //Test it out
+  collatz(84);
+  cout << endl;
+  collatz(179);
+  cout << endl;
+}
+
+//Run collatz sequence on n
+void collatz(int n) {
+  cout << n << " ";
+  if(n == 1) return;
+
+  if(n%2==0) collatz(n / 2);
+  else collatz(3 * n + 1);
 }
