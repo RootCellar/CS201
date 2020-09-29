add32b4287ef3ca8646c8eeb04202f27e44c6a3e
RootCellar <rootcellar9877@gmail.com>
2020-09-28 18:40:10 -0800

    Complete main and solve method to solve the quadratic and give the user the answer

--- HW/HW3/quadratic.cpp
+++ HW/HW3/quadratic.cpp
@@ -6,12 +6,14 @@
 */
 
 #include <iostream>
+#include <cmath>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 bool canSolve(double a, double b, double c);
+void solve(double a, double b, double c, double & sol1, double & sol2);
 
 int main() {
   double a, b, c;
@@ -35,8 +37,25 @@ int main() {
     return 1;
   }
 
+  double solution1, solution2 = 0;
 
+  solve(a, b, c, solution1, solution2);
 
+  cout << "Solution 1: " << solution1 << endl;
+  cout << "Solution 2: " << solution2 << endl;
+}
+
+void solve(double a, double b, double c, double & sol1, double & sol2) {
+  //Let's just make sure this is possible first...
+  if(!canSolve(a,b,c)) return;
+
+  double root = sqrt( pow(b,2) - 4 * a * c );
+
+  sol1 = ( -1 * b ) + root;
+  sol1 /= 2 * a;
+
+  sol2 = ( -1 * b ) - root;
+  sol2 /= 2 * a;
 }
 
 //Just check if this equation has any real roots
