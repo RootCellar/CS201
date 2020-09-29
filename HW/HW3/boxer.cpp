6af9377804666a9fcfb347f1253c9063118252db
RootCellar <rootcellar9877@gmail.com>
2020-09-28 17:16:41 -0800

    Function to help with line legth

--- HW/HW3/boxer.cpp
+++ HW/HW3/boxer.cpp
@@ -6,3 +6,19 @@
 */
 
 #include "HW3.h"
+
+//Figure out how long each line will be.
+//Useful mostly for the lines of asterisks preceding the words
+int getLineLength(string words, int layers) {
+
+  //length of string
+  int total = words.size();
+
+  //space to edge, one space on either side
+  total += 2;
+
+  //Rest of asterisks on either side
+  total += layers * 2;
+
+  return total;
+}
