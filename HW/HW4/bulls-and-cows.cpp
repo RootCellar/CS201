0c54ce51b67e9dc23e77aaa7868ad7f3a79a2bfa
RootCellar <rootcellar9877@gmail.com>
2020-10-08 17:51:54 -0800

    finish main in bulls-and-cows.cpp, and fix up other functions that weren't quite right.

--- HW/HW4/bulls-and-cows.cpp
+++ HW/HW4/bulls-and-cows.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
@@ -15,27 +16,79 @@ using std::cin;
 
 using std::vector;
 using std::string;
+using std::reverse;
 
-void clear(vector<int> v);
+void clear(vector<int> &v);
 bool intoVector(int i, vector<int>& v);
 void bullsncows(int& bulls, int& cows, vector<int>& guess, vector<int>& answer);
 
 int main() {
   vector<int> answer = {3,6,8,1};
+  //reverse(answer.begin(), answer.end());
   vector<int> guess;
   int entry;
+  int bulls = 0, cows = 0;
+
+  cout << "Welcome to bulls and cows!" << endl <<
+  "The object of the game is to guess a number." << endl <<
+  "1 bull means you got a number right and in the right spot" << endl <<
+  "1 cow means you got a number right, not necessarily in the right spot" << endl <<
+  "Have Fun!" << endl
+  ;
+
+  cout << endl << endl << endl;
+
+  while(true) {
+    clear(guess);
+    cout << "Enter a number as your guess: ";
+    cin >> entry;
+    cout << endl;
+
+    if(entry < 0) {
+      for(int n : answer) cout << n;
+      cout << endl;
+      continue;
+    }
+
+    bool valid = intoVector(entry, guess);
+    if(!valid) {
+      cout << "Sorry, your guess was not 4 digits! Please try again." << endl;
+      continue;
+    }
+
+    reverse(guess.begin(), guess.end());
+    bulls = 0;
+    cows = 0;
+    bullsncows(bulls, cows, guess, answer);
+    cout << bulls << " bulls, " << cows << " cows." << endl;
+
+    if(bulls == 4) {
+      cout << "YOU WON!" << endl;
+      return 1;
+    }
+
+  }
+
 }
 
 //Find bulls and cows
 void bullsncows(int& bulls, int& cows, vector<int>& guess, vector<int>& answer) {
 
   for(int i = 0; i < guess.size(); i++) {
-
     int g = guess[i];
-    
+
+    cout << g << endl;
+
     if(g == answer[i]) bulls++;
     else {
-      for(int n : answer) if( g==n ) cows++;
+
+      bool done = false;
+      for(int n : answer)
+        if( !done && g==n ) {
+          cows++;
+          done = true;
+        }
+
     }
 
   }
@@ -48,7 +101,7 @@ bool intoVector(int i, vector<int>& v) {
   while(i > 0) {
     int digit = i % 10;
     i /= 10;
-    v.push_back(i);
+    v.push_back(digit);
     digits++;
   }
 
@@ -56,7 +109,7 @@ bool intoVector(int i, vector<int>& v) {
 }
 
 //Clear the given vector of ints
-void clear(vector<int> v) {
+void clear(vector<int> &v) {
   while(v.size() > 0) {
     v.pop_back();
   }
