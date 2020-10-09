963360f5e7b8b2be7cba7dab98990d7b75f71c32
RootCellar <rootcellar9877@gmail.com>
2020-10-08 18:47:18 -0800

    Move into separate folders; use makefile

---
+++ HW/HW4/crud/crud.cpp
@@ -0,0 +1,6 @@
+/*
+ * Darian Marvel
+ * 10/8/2020
+ * crud.cpp
+ * implementing CRUD functions for a database
+*/
---
+++ HW/HW4/crud/database.cpp
@@ -0,0 +1,6 @@
+/*
+ * Darian Marvel
+ * 10/8/2020
+ * database.cpp
+ * Using crud.cpp
+*/
