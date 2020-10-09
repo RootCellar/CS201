0a5c2269614fd6df6e13b3fd96fe9de1f9418206
RootCellar <rootcellar9877@gmail.com>
2020-10-08 17:15:50 -0800

    write intoVector in bulls-and-cows.cpp

--- HW/HW4/bulls-and-cows.cpp
+++ HW/HW4/bulls-and-cows.cpp
@@ -17,10 +17,24 @@ using std::vector;
 using std::string;
 
 void clear(vector<int> v);
+bool intoVector(int i, vector<int>& v);
 
 int main() {
   vector<int> answer = {3,6,8,1};
   vector<int> guess;
+}
+
+//Place an int into a vector, returning if the int has 4 digits
+bool intoVector(int i, vector<int>& v) {
+  int digits = 0;
+  while(i > 0) {
+    int digit = i % 10;
+    i /= 10;
+    v.push_back(i);
+    digits++;
+  }
+
+  return digits==4;
 }
 
 //Clear the given vector of ints
