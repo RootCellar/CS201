21350c87c571a4d92ae2451843cb152f4b263f5d
RootCellar <RootCellar9877@GMail.com>
2020-11-01 13:37:11 -0900

    improve build script

--- HW/HW5/truncstruct/build.sh
+++ HW/HW5/truncstruct/build.sh
@@ -1,9 +1,4 @@
 #Base command for compiling with fltk installed looks like this:
 #fltk-config --compile *.cxx
 
-cd truncstruct
-./build.sh
-
-cd ..
-cd bullsncows
-fltk-config --compile *.cpp
+fltk-config --compile main.cpp
