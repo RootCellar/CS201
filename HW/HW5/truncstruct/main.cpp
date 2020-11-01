f1ff37f73cd2624e876e56a9e4e46e2dbce918b4
RootCellar <RootCellar9877@GMail.com>
2020-11-01 13:35:51 -0900

    setup FLTK usage

--- HW/HW5/truncstruct/main.cpp
+++ HW/HW5/truncstruct/main.cpp
@@ -7,8 +7,35 @@
 */
 
 //Includes
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Overlay_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/fl_draw.H>
 
+#include "truncstruct.hpp"
 
-int main() {
+//Function Prototypes
+void run(Fl_Widget *,void *);
+
+//Data
+Fl_Window *window;
+
+int arg(int, char **argv, int& i) {
+  return i;
+}
+
+int main(int argc, char **argv) {
+  int i = 0;
+  Fl::args(argc,argv,i,arg);
+  window = new Fl_Window(500, 500);
+  Fl_Button *button;
+  button = new Fl_Button(50,300,400,100,"Enter");
+  button->callback(run);
+  window->show(argc,argv);
+  return Fl::run();
+}
+
+void run(Fl_Widget *,void *) {
 
 }
