df825d167927623b52f4aa498b3d62ef52a2ce8a
RootCellar <RootCellar9877@GMail.com>
2020-11-29 19:15:43 -0900

    Setup Project

---
+++ Project/Client/Client.cpp
@@ -0,0 +1,10 @@
+/*
+ * Darian Marvel
+ * 11/25/2020
+ * Client.cpp
+ * Handling the client side for a chat server
+*/
+
+int main() {
+  
+}
---
+++ Project/Server/Server.cpp
@@ -0,0 +1,10 @@
+/*
+ * Darian Marvel
+ * 11/25/2020
+ * Server.cpp
+ * Handling the server side for a chat server
+*/
+
+int main() {
+
+}
