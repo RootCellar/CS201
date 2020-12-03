220a76be2793b880551e9a5e699ae95313355dc6
RootCellar <RootCellar9877@GMail.com>
2020-12-03 10:37:16 -0900

    Write main for Server.cpp

--- Project/Chat.h
+++ Project/Chat.h
@@ -1,14 +1,33 @@
 #ifndef CHAT_H
 #define CHAT_H
 
+//Defines
+
+//Defining this so client always knows what port to connect to,
+//and server always knows where to bind the socket
+#define PORT 8000
+
+#define BUFFER_SIZE 1024
+
+//Normal C++ Headers
 #include <thread>
 #include <iostream>
+#include <string>
 
+//C Headers - C++ doesn't seem to have it's own socket headers
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+//Using statements
 using std::cout;
 using std::cin;
 using std::endl;
 
+using std::string;
+using std::to_string;
+
 using std::thread;
+
 
 class SocketHandler
 {
--- Project/Server/Server.cpp
+++ Project/Server/Server.cpp
@@ -8,5 +8,54 @@
 #include "../Chat.h"
 
 int main() {
+  int serverSocket, clientSocket, valread;
+  sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[BUFFER_SIZE] = {0};
+  string message = "Hello! I have received your connection.";
+
+  serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(serverSocket == 0) {
+    cout << "Can't create socket" << endl;
+    exit(0);
+  }
+
+  bool sockOpt = setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+  if(sockOpt) {
+    cout << "Can't set socket options" << endl;
+    exit(0);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( PORT );
+
+  int bound = bind( serverSocket, (sockaddr *) &address, sizeof(address) );
+
+  if(bound < 0) {
+    cout << "Can't bind the socket" << endl;
+    exit(0);
+  }
+
+  int didListen = 0;
+  while(true) {
+    didListen = listen(serverSocket, 3);
+
+    if(didListen < 0) {
+      cout << "Can't listen on the socket" << endl;
+      exit(0);
+    }
+
+    clientSocket = accept(serverSocket, (sockaddr *) &address, (socklen_t*) &addrlen);
+
+    if(clientSocket < 0) {
+      cout << "Can't accept client socket" << endl;
+      exit(0);
+    }
+
+    send(clientSocket, message.c_str(), message.size(), 0);
+
+  }
 
 }
